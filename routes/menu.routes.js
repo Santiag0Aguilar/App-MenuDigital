9a91381d6c4114de5f61b107329a72700f884736
Santiago Aguilar <aguilarsantiagoguil2007@gmail.com>
2026-02-10 12:06:18 -0600

    Agregando el price al mapeo de la informacion de loyverse, queda como pendiente manejar el nuevo campo en neon y mandar migracion

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 import MenuValidateMiddleware from "../middleware/updateMenu.middleware.js";
 import MenuUpdateController from "../controller/updateMenu.controller.js";
+import authMiddelware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+router.get("/", authMiddelware);
 router.post("/update", MenuValidateMiddleware, MenuUpdateController);
-
 export default router;
