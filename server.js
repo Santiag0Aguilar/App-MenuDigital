cbcf0599ade7e7d84c130cf6cd1871a77b3d2029
Santiago Aguilar <130328638+Santiag0Aguilar@users.noreply.github.com>
2026-02-12 13:46:34 -0600

    Add localhost:3001 to allowed origins

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:3001",
   "http://localhost:4173",
   "http://localhost:5173",
   "https://app-menudigital.netlify.app",
